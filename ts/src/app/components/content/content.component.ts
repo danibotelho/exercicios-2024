027279cbfdc58a5f8334b77bc326471f68b6a81b
danibotelho <danibl26@gmail.com>
2024-05-03 20:03:57 -0300

    adicionando componentes no content component

--- ts/src/app/components/content/content.component.ts
+++ ts/src/app/components/content/content.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  discussoes = [
+    {
+      assunto: 'O que é Angular ',
+      autor: 'Carlos Henrique Santos',
+      pergunta: 'Comecinho da pergunta aparece aqui resente relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo... ',
+    },
+    {
+      assunto: 'Component e uma Directive',
+      autor: 'John Doe' ,
+      pergunta: 'Qual a diferença entre um Component e uma Directive? ',
+    }
+  ];
 }
