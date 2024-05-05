265620e231709a14049e0541e2e097a34e6c53a3
danibotelho <danibl26@gmail.com>
2024-05-05 14:18:39 -0300

    alteracoes finais

--- ts/src/app/components/content/content.component.ts
+++ ts/src/app/components/content/content.component.ts
@@ -16,24 +16,16 @@ export class ContentComponent implements OnInit {
   mostrarCardCriado: boolean = false;
   mostrarMsgTopicoCriado: boolean = false;
 
-
   ngOnInit(): void {}
 
   discussoes: FormDiscussao[] = [
     {
-      assunto: 'O que é Angular ',
+      assunto: 'O que é Angular',
       autor: 'Carlos Henrique Santos',
       pergunta:
         'Comecinho da pergunta aparece aqui resente relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo... ',
-    },
-    {
-      assunto: 'Component e uma Directive',
-      autor: 'John Doe',
-      pergunta: 'Qual a diferença entre um Component e uma Directive? ',
-    },
+    }
   ];
-
-
 
   alterarComponente() {
     this.mostrarCriar = !this.mostrarCriar;
